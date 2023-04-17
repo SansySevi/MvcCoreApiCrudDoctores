cbdf86e11bfc7d3fb0bd9f56ddd7b749bbd856c7
SansySevi <ssevilleja@hotmail.es>
2023-04-17 14:18:37 +0200

    Add project files.

---
+++ MvcCoreApiCrudDoctores/Controllers/DoctoresController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreApiCrudDoctores.Services;
+
+namespace MvcCoreApiCrudDoctores.Controllers
+{
+    public class DoctoresController : Controller
+    {
+        private ServiceApiDoctores service;
+
+        public DoctoresController(ServiceApiDoctores service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
